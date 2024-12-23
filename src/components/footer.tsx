99a48c5aea85e932ade70931c7835f3ddfceaa04
cpyext <cpurighalla@yext.com>
2024-12-23 22:42:10 +0530

    init files - cp

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+const Footer = ({ _site }: any) => {
+  return (
+    <img src="https://a.mktgcdn.com/p-sandbox/_wdgRtEzrBebVyoUXCb4uQ0MzwBsEFZ1vzdg_QcgevE/2854x1136.png" className="w-full" alt="" />
+  );
+};
+
+export default Footer;
